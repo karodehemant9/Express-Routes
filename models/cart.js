b567d0456083fad3bfb0beb0128cd3a0ec22da5e
karodehemant9 <karode.hemant9@gmail.com>
2023-10-07 22:18:47 +0530

    adding delete product functionality

--- models/cart.js
+++ models/cart.js
@@ -7,71 +7,68 @@ const p = path.join(
   'cart.json'
 );
 
-// const getProductsFromFile = cb => {
-//   fs.readFile(p, (err, fileContent) => {
-//     if (err) {
-//       cb([]);
-//     } else {
-//       cb(JSON.parse(fileContent));
-//     }
-//   });
-// };
-
 module.exports = class Cart {
-  static addProduct(id, productPrice){
-    //Fetch the existing state of the cart
+  static addProduct(id, productPrice) {
+    // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
-        let cart = {products: [], totalPrice: 0};
-        if (!err) {
-          cart = JSON.parse(fileContent);
-        }
-        //Analyse the cart => find existing product
-
-        const existingProductIndex = cart.products.find(prod => prod.id === id);
-        const existingProduct = cart.products[existingProductIndex];
-        console.log(existingProduct);
-        let updatedProduct;
-        //add new product to cart/or increase the quentity
-        if(existingProduct){
-            updatedProduct = {...existingProduct};
-            updatedProduct.quantity = updatedProduct.quantity + 1;
-            cart.products = [...cart.products];
-            cart.products[existingProductIndex] = updatedProduct;
-            console.log(updatedProduct);
-        }
-        else{
-            updatedProduct = {id: id, quantity: 1};
-            cart.products = [...cart.products, updatedProduct];
-        }
-        cart.totalPrice = cart.totalPrice + productPrice;
-        fs.writeFile(p, JSON.stringify(cart), (err)=>{
-            console.log(err);
-        })
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(
+        prod => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+      // Add new product/ increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), err => {
+        console.log(err);
       });
-
-    
-
-    
+    });
   }
 
-//   save() {
-//     this.id = Math.random().toString();
-//     getProductsFromFile(products => {
-//       products.push(this);
-//       fs.writeFile(p, JSON.stringify(products), err => {
-//         console.log(err);
-//       });
-//     });
-//   }
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id);
+      if (!product) {
+          return;
+      }
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        prod => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
 
-//   static fetchAll(cb) {
-//     getProductsFromFile(cb);
-//   }
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    });
+  }
 
-//   static findById(id, cb) {
-//     getProductsFromFile(products => {
-//       const product = products.find(p => p.id === id);
-//       cb(product);
-//     });
-//   }
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
+  }
 };

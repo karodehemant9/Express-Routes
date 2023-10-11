6951eaef929bf6bcadb4a5b5e469ab79f99d2778
karodehemant9 <karode.hemant9@gmail.com>
2023-10-11 14:31:10 +0530

    defining relationships between models

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const path = require('path');
+const Sequelize = require('sequelize');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
-
-const sequelize = require('./util/database.js');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -20,19 +20,44 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) =>{
+    User.findByPk(1)
+    .then(user =>{
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
-.then(result =>{
+
+
+
+//establishing relationship between models
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'}); //i.e. A user created this product
+User.hasMany(Product);
+
+sequelize
+  .sync()
+  .then(result => {
+    return User.findByPk(1)
+    // console.log(result);
+    
+  })
+  .then(user =>{
+    if(!user){
+        return User.create({name: 'Hemant', email: 'karode.hemant9@gmail.com'});
+    }
+    return Promise.resolve(user);
+  })
+  .then(user =>{
+    //console.log(user);
     app.listen(3000);
-})
-.catch(err =>{
+  })
+  .catch(err => {
     console.log(err);
-})
-//synchronize your models to your DB tables: creat table if table doesn't exist
-
-
-
+  });
--- models/cart.js
+++ models/cart.js
@@ -1,4 +1,11 @@
-const db = require('../util/database.js');
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
 
 module.exports = class Cart {
   static addProduct(id, productPrice) {
